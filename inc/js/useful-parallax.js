91dda3b25dfd6f6ea9f83976ce21bbba93399b6e
Maurice van Creij <maurice.vancreij@webqem.com>
2014-12-04 09:26:56 +1100

    Update demo code

--- inc/js/useful-parallax.js
+++ inc/js/useful-parallax.js
@@ -387,178 +387,90 @@ var useful = useful || {};
 })();
 
 /*
-
 	Source:
-
 	van Creij, Maurice (2014). "useful.parallax.js: Position and load backgrounds based on screen position.", version 20141127, http://www.woollymittens.nl/.
 
-
-
 	License:
-
 	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
-
 */
 
-
-
 // create the constructor if needed
-
 var useful = useful || {};
-
 useful.Parallax = useful.Parallax || function () {};
 
-
-
 // extend the constructor
-
 useful.Parallax.prototype.init = function (cfg) {
-
 	// properties
-
 	"use strict";
-
 	this.cfg = cfg;
-
 	this.scroller = cfg.scroller || window;
-
 	this.foregrounds = cfg.foregrounds;
-
 	this.backgrounds = cfg.backgrounds;
-
 	// methods
-
 	this.start = function () {
-
 		// set any default values if there wasn't one
-
 		this.cfg.vertical = this.cfg.vertical || 1;
-
 		this.cfg.offset = this.cfg.offset || 0;
-
 		this.cfg.exponential = this.cfg.exponential || 1;
-
 		this.cfg.treshhold = this.cfg.treshhold || 100;
-
 		this.cfg.parallax = this.cfg.parallax || false;
-
 		// set the default repeat behaviour
-
 		this.cfg.always = this.cfg.always || false;
-
 		// set the scrolling event handler
-
 		this.scroller.addEventListener('scroll', this.onUpdate(), true);
-
 		// perform the first redraw
-
 		this.update();
-
 		// disable the start function so it can't be started twice
-
 		this.start = function () {};
-
 	};
-
 	this.update = function () {
-
 		var objectPos, objectSize, relativePosition, className,
-
 			replace = new RegExp(' off-stage| on-stage|off-stage|on-stage', 'i'),
-
 			relativeOffset = this.cfg.offset / 100,
-
 			relativeDisplacement = this.cfg.displacement / 100;
-
 		// get the scroll position
-
 		var scrollSize = useful.positions.window(this.scroller);
-
 		var scrollPos = useful.positions.document(this.scroller);
-
 		// if we can measure the scroller
-
 		if (scrollSize.y !== 0) {
-
 			// for every watched element
-
 			for (var a = 0, b = this.foregrounds.length, c = this.backgrounds.length; a < b; a += 1) {
-
 				className = this.foregrounds[a].className;
-
 				// get the object position / dimensions
-
 				objectPos = { x : this.foregrounds[a].offsetLeft, y : this.foregrounds[a].offsetTop };
-
 				objectSize = { x : this.foregrounds[a].offsetWidth, y : this.foregrounds[a].offsetHeight };
-
 				// if the object is in the viewport
-
 				if (objectPos.y + objectSize.y >= scrollPos.y - this.cfg.offset && objectPos.y < scrollPos.y + this.cfg.treshold + scrollSize.y) {
-
 					// if required position the parallax
-
 					if (b === c) {
-
 						relativePosition = (objectPos.y - scrollPos.y + objectSize.y) / (scrollSize.y + objectSize.y);
-
 						relativePosition = Math.min(Math.max(relativePosition, 0), 1);
-
 						relativePosition = Math.pow((relativePosition - 0.5) * 2, this.cfg.exponential);
-
 						relativePosition = relativePosition * relativeDisplacement + relativeOffset;
-
 						this.backgrounds[a].style.transform = 'translateY(' + (relativePosition * 100) + '%)';
-
 						this.backgrounds[a].style.webkitTransform = 'translateY(' + (relativePosition * 100) + '%)';
-
 						this.backgrounds[a].style.msTransform = 'translateY(' + (relativePosition * 100) + '%)';
-
 					}
-
 					// mark its visibility
-
 					this.foregrounds[a].className = className.replace(replace, '') + ' on-stage';
-
 				} else {
-
 					// mark the object is outside the viewport
-
 					this.foregrounds[a].className = className.replace(replace, '') + ' off-stage';
-
 				}
-
 			}
-
 		}
-
 	};
-
 	// events
-
 	this.onUpdate = function () {
-
 		var _this = this;
-
 		return function () { _this.update(); };
-
 	};
-
 	// go
-
 	this.start();
-
 	return this;
-
 };
 
-
-
 // return as a require.js module
-
 if (typeof module !== 'undefined') {
-
 	exports = module.exports = useful.Parallax;
-
 }
-
